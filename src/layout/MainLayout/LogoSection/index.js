ddac048532ad3d1779d19d049813b34e28ca5ea3
syahruncega <syahruncega@gmail.com>
2023-01-13 14:12:09 +0800

    New Logo

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -5,17 +5,19 @@ import { Box, Link as MuiLink, Typography } from '@mui/material';
 // project imports
 import { DASHBOARD_PATH } from 'config';
 import Logo from 'components/ui-component/Logo';
+import Image from 'next/image';
+import { maxWidth } from '@mui/system';
 
 // ==============================|| MAIN LOGO ||============================== //
 
 const LogoSection = () => (
   <MuiLink component={Link} href={DASHBOARD_PATH} sx={{ textDecoration: 'none' }}>
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+    <Box sx={{ display: 'flex', alignItems: 'center', maxWidth: '160px' }}>
       <Logo />
-      <Box sx={{ ml: '8px' }}>
+      {/* <Box sx={{ ml: '8px' }}>
         <Typography sx={{ color: 'black', fontWeight: '900', fontSize: 13 }}>APLIKASI</Typography>
         <Typography sx={{ color: 'black', fontWeight: '900', fontSize: 13 }}>KEMISKINAN</Typography>
-      </Box>
+      </Box> */}
     </Box>
   </MuiLink>
 );
