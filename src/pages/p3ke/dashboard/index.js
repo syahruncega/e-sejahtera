d2c232cab12eb292df0512605a82fc0fb02a7b93
syahruncega <syahruncega@gmail.com>
2023-02-20 04:28:47 +0800

    Dashboard Almost Done

--- src/pages/p3ke/dashboard/index.js
+++ src/pages/p3ke/dashboard/index.js
@@ -4,6 +4,9 @@ import Page from 'components/ui-component/Page';
 import DashboardP3KEAdmin from 'components/dashboard/P3KE/Beranda/Admin/DashboardP3KEAdmin';
 import useAuth from 'hooks/useAuth';
 import DashboardP3KEMahasiswa from 'components/dashboard/P3KE/Beranda/Mahasiswa/DashboardP3KEMahasiswa';
+import DashboardP3KEDosen from 'components/dashboard/P3KE/Beranda/Dosen/DashboardP3KEDosen';
+import DashboardP3KEPusbang from 'components/dashboard/P3KE/Beranda/Pusbang/DashboardP3KEPusbang';
+import DashboardP3KEAnalis from 'components/dashboard/P3KE/Beranda/Analis/DashboardP3KEAnalis';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
@@ -11,7 +14,11 @@ const Dashboard = () => {
   const { user } = useAuth();
   return (
     <Page title="Dashboard P3KE">
-      {user.role === 'admin' && <DashboardP3KEAdmin />} {user.role === 'mahasiswa' && <DashboardP3KEMahasiswa />}
+      {user.role === 'admin' && <DashboardP3KEAdmin />}
+      {user.role === 'mahasiswa' && <DashboardP3KEMahasiswa />}
+      {user.role === 'dosen' && <DashboardP3KEDosen />}
+      {user.role === 'pusbang' && <DashboardP3KEPusbang />}
+      {user.role === 'analis' && <DashboardP3KEAnalis />}
     </Page>
   );
 };
